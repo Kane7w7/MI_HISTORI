fe99e3cf51206ce3c59c563b68a7a9d0bd025ce5
Kane7w7 <154647390+Kane7w7@users.noreply.github.com>
2024-01-03 02:48:50 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let x = document.getElementById('Fin').value;
+
+x.addEventListener(mensaje);
+
+function mensaje(){
+    alert('Esta fue la historia de estos casi "dos años" de los cuales quiero que seoas por lo que tube que pasar hasta este momento y dejar esa historia atras');
+}
